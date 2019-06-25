d778bbd7ed43b1373a4493637c5aadf1a68508fd
yangqinjiang <531553280@qq.com>
2019-06-25 22:56:39 +0800

    fix bug

--- web/gitium_tools_webapp/index.js
+++ web/gitium_tools_webapp/index.js
@@ -2,11 +2,11 @@ const createSeed = require('gitium.seed.js');
 const express = require('express')
 const app = express()
 //静态目录
-app.use('/html',express.static('html'))
+app.use('/html', express.static('html'))
 app.get('/', (req, res) => res.send('This is gitium tools! <a href="/html/index.html">点击这里打开</a>'));
 //生成 gitium_seed
 app.post('/api/create_seed', function (req, res) {
-    var seed = createSeed.createSeed(req.query.name || '',req.query.pass || '');
-    res.send(seed);
-  })
+  var seed = createSeed.createSeed(req.query.name, req.query.pass);
+  res.send(seed);
+})
 app.listen(3000, () => console.log('app listening on port 3000!'))
